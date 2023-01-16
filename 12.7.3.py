9339e1c72b60db367b59d4082cb1e655fa403305
Ivan41945 <122258970+Ivan41945@users.noreply.github.com>
2023-01-16 20:18:14 +0300

    Add files via upload

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,10 +1,10 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money=float(input())
 
-per_cent['ТКБ']=round(5.6 * money)
-per_cent['СКБ']=round(5.9 * money)
-per_cent['ВТБ']=round(4.28 * money)
-per_cent['СБЕР']=round(4.0 * money)
+per_cent['ТКБ']=round(0.056*money)
+per_cent['СКБ']=round(0.059 * money)
+per_cent['ВТБ']=round(0.0428 * money)
+per_cent['СБЕР']=round(0.04 * money)
 
 print('список deposit значений',per_cent['ТКБ'],per_cent['СКБ'],per_cent['ВТБ'],per_cent['СБЕР'])
 print('Максимальная сумма, которую вы можете заработать — ',max(per_cent.values()))
